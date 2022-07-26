ebc4139a291d6de709864e023f42a5a08eb69282
scaulfield7 <scaulfield@live.ie>
2022-07-26 15:50:17 +0100

    Change conversion from String to char

--- src/converter/height/Main.java
+++ src/converter/height/Main.java
@@ -4,15 +4,15 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-	    String conversion;
+	    char conversion;
 		Converter converter = new Converter();
 		System.out.println("Enter \"c\" to convert from feet and inches to centimetres. Enter \"f\" to convert from centimetres to feet and inches.");
-		conversion = input.next();
+		conversion = input.next().charAt(0);
 
-		if(conversion.equals("c")){
+		if(conversion == 'c'){
 			converter.convertToCm();
 		}
-		else if(conversion.equals("f")){
+		else if(conversion == 'f'){
 			converter.convertToFeetAndInches();
 		}
     }
